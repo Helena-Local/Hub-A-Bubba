141eabd98efa10b3b8ff26a0795d45dac4c59444
Kevin <kcolussi@gmail.com>
2014-01-27 20:37:18 -0700

    Refactored to allow for csv and xml... Then ditched the idea of xml... just overly complicated here.... I'll pull the code after this is pushed...

--- src/org/helenalocal/base/Hub.java
+++ src/org/helenalocal/base/Hub.java
@@ -3,6 +3,9 @@ package org.helenalocal.base;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Xml;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -15,7 +18,10 @@ import java.util.Iterator;
  */
 public abstract class Hub implements IHub {
     private String filename ="hl-out.txt";
+    protected String dataUrl = "";
     protected Calendar lastRefreshTS;
+    protected static final int CSV = 1;
+    protected static final int RSS = 2;
     public static final int CSA = 1;
     public static final int GROWER = 2;
     public static final int SALES = 3;
@@ -23,35 +29,171 @@ public abstract class Hub implements IHub {
     public static final String BACKEND = "BACKEND";
     public static final String FRONTEND = "FRONTEND";
 
-    private Product createProduct(String receiveString) {
-        Product out = new Product();
+    private void parseCSV(ArrayList<Product> myProducts, InputStream inputStream) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         TextUtils.SimpleStringSplitter simpleStringSplitter = new TextUtils.SimpleStringSplitter(',');
-        simpleStringSplitter.setString(receiveString);
-        Iterator<String> iterator = simpleStringSplitter.iterator();
+        String receiveString = "";
+        boolean firstTime = true;
+        while ( (receiveString = bufferedReader.readLine()) != null ) {
+            if (firstTime) {
+                // remove header
+                firstTime = false;
+            } else {
+                // build products
+                Product out = new Product();
+                simpleStringSplitter.setString(receiveString);
+                Iterator<String> iterator = simpleStringSplitter.iterator();
 
-        // String productDesc, Integer unitsAvailable, Double unitPrice, String unitDesc, String note
-        if (iterator.hasNext()) {
-            out.setProductDesc(iterator.next());
+                // String category, productDesc, Integer unitsAvailable, Double unitPrice, String unitDesc, String note
+                if (iterator.hasNext()) {
+                    String category = iterator.next();
+                    if (! category.equals("")) {
+                        out.setCategory(category);
+                    }
+                }
+                if (iterator.hasNext()) {
+                    String productDesc = iterator.next();
+                    if (! productDesc.equals("")) {
+                        out.setProductDesc(productDesc);
+                    }
+                }
+                if (iterator.hasNext()) {
+                    String unitsAvailable = iterator.next();
+                    if (! unitsAvailable.equals("")) {
+                        out.setUnitsAvailable(Integer.valueOf(unitsAvailable));
+                    }
+                }
+                if (iterator.hasNext()) {
+                    String unitPrice = iterator.next();
+                    if (! unitPrice.equals("")) {
+                        Object o = unitPrice.replace("$", "");
+                        out.setUnitPrice(Double.parseDouble(o.toString()));
+                    }
+                }
+                if (iterator.hasNext()) {
+                    String unitDesc = iterator.next();
+                    if (! unitDesc.equals("")) {
+                        out.setUnitDesc(unitDesc);
+                    }
+                }
+                if (iterator.hasNext()) {
+                    String note = iterator.next();
+                    if (! note.equals("")) {
+                        out.setNote(note);
+                    }
+                }
+                myProducts.add(out);
+            }
         }
-        if (iterator.hasNext()) {
-            out.setUnitsAvailable(Integer.valueOf(iterator.next()));
-        }
-        if (iterator.hasNext()) {
-            Object o = iterator.next().replace("$", "");
-            out.setUnitPrice(Double.parseDouble(o.toString()));
-        }
-        if (iterator.hasNext()) {
-            out.setUnitDesc(iterator.next());
-        }
-        if (iterator.hasNext()) {
-            out.setNote(iterator.next());
-        }
-        return out;
     }
+
+    /* decided not to use XML... Overly complicated for this requirement.
+    private void parseRSS(ArrayList<Product> myProducts, InputStream inputStream) throws IOException,XmlPullParserException {
+        XmlPullParser parser = Xml.newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(inputStream, null);
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, null, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("entry")) {
+                // myProducts.add(readEntry(parser));
+                Object o;
+                String lastUpdated = "";
+                String title = "";
+                String link = "";
+                while (parser.next() != XmlPullParser.END_TAG) {
+                    if (parser.getEventType() != XmlPullParser.START_TAG) {
+                        continue;
+                    }
+                    String name2 = parser.getName();
+                    if (name2.equals("title")) {
+                        title = readText(parser);
+                    } else if (name2.equals("lastUpdated")) {
+                        lastUpdated = readText(parser);
+                    } else if (name2.equals("sites-layout-tile sites-tile-name-content-1")) {
+                        link = readText(parser);
+                    } else {
+                        skip(parser);
+                    }
+                }
+            } else {
+                skip(parser);
+            }
+        }
+        //// STOPPED!
+        while ( (receiveString = bufferedReader.readLine()) != null ) {
+            // build products
+            Product out = new Product();
+            simpleStringSplitter.setString(receiveString);
+            Iterator<String> iterator = simpleStringSplitter.iterator();
+
+            // String productDesc, Integer unitsAvailable, Double unitPrice, String unitDesc, String note
+            if (iterator.hasNext()) {
+                out.setProductDesc(iterator.next());
+            }
+            if (iterator.hasNext()) {
+                out.setUnitsAvailable(Integer.valueOf(iterator.next()));
+            }
+            if (iterator.hasNext()) {
+                Object o = iterator.next().replace("$", "");
+                out.setUnitPrice(Double.parseDouble(o.toString()));
+            }
+            if (iterator.hasNext()) {
+                out.setUnitDesc(iterator.next());
+            }
+            if (iterator.hasNext()) {
+                out.setNote(iterator.next());
+            }
+            myProducts.add(out);
+        }
+
+    }
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+    */
 
     protected void setFilename(String filename) {
         this.filename = filename;
     }
+
+    protected String getDataUrl() {
+        return dataUrl;
+    }
+
+    protected void setDataUrl(String dataUrl) {
+        this.dataUrl = dataUrl;
+    }
+
 
     protected void writeToFile(Context context, String data) {
         try {
@@ -76,7 +218,7 @@ public abstract class Hub implements IHub {
         }
     }
 
-    protected ArrayList<Product> readFromFile(Context context) {
+    protected ArrayList<Product> readFromFile(Context context, int itype) {
         ArrayList<Product> myProducts = new ArrayList<Product>();
         try {
             // get the time the file was last changed here
@@ -89,25 +231,22 @@ public abstract class Hub implements IHub {
             // create products from the file here
             InputStream inputStream = context.openFileInput(filename);
             if ( inputStream != null ) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                boolean firstTime = true;
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    if (firstTime) {
-                        // remove header
-                        firstTime = false;
-                    } else {
-                        // build products
-                        myProducts.add(createProduct(receiveString));
-                    }
+                if (itype == CSV) {
+                    parseCSV(myProducts, inputStream);
                 }
+                /*
+                else if (itype == RSS) {
+                    parseRSS(myProducts, inputStream);
+                }
+                */
                 inputStream.close();
             }
         } catch (FileNotFoundException e) {
             Log.e(Hub.BACKEND, "File  (" + filename + ") not found: " + e.toString());
         } catch (IOException e) {
             Log.e(Hub.BACKEND, "Can not read file  (" + filename + ") : " + e.toString());
+        // } catch (XmlPullParserException e) {
+        //    Log.e(Hub.BACKEND, "Can not parse XML file  (" + filename + ") : " + e.toString());
         }
         Log.w(Hub.BACKEND, "Number of products loaded: " + myProducts.size());
         return myProducts;
