a8355c014afd5033f8205acfb191246d3982206f
Kevin <kcolussi@gmail.com>
2014-02-06 12:21:35 -0700

    impled onPause and onResume for Hub get thread actions... All tested and working... just need to compare ui load time with hub last modified (getTime()) to see if the UI needs to redraw the hash values-- Kevin

--- src/org/helenalocal/app/MainActivity.java
+++ src/org/helenalocal/app/MainActivity.java
@@ -1,49 +1,70 @@
+/*
+ * Copyright (c) 2014. This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License for Helena Local Inc. All rights reseved.
+ */
+
 package org.helenalocal.app;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import org.helenalocal.Helena_Local_Hub.R;
-import org.helenalocal.base.*;
+import org.helenalocal.base.Hub;
 import org.helenalocal.base.get.BuyerHub;
 import org.helenalocal.base.get.ItemHub;
 import org.helenalocal.base.get.OrderHub;
 import org.helenalocal.base.get.ProducerHub;
-import org.helenalocal.base.post.GrowerHub;
 import org.helenalocal.utils.ViewServer;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity {
+    private static ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(4);
+    ScheduledFuture buyerHubScheduledFuture;
+    ScheduledFuture<?> itemHubScheduledFuture;
+    ScheduledFuture<?> orderHubScheduledFuture;
+    ScheduledFuture<?> producerHubScheduledFuture;
 
     private static final String Tag = "MainActivity";
 
-    private static void initGetHubs(Context _context) {
+    private void startHubThreads() {
+        Log.w(Tag, "startHubThreads exec.getQueue().size() = " + exec.getQueue().size());
         // schedule hub refreshes...
-        ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(4);
-        exec.scheduleWithFixedDelay(new BuyerHub(_context), 0, Hub.buyerDelay, TimeUnit.MINUTES);
-        exec.scheduleWithFixedDelay(new ItemHub(_context), 0, Hub.itemDelay, TimeUnit.MINUTES);
-        exec.scheduleWithFixedDelay(new OrderHub(_context), 0, Hub.orderDelay, TimeUnit.MINUTES);
-        exec.scheduleWithFixedDelay(new ProducerHub(_context), 0, Hub.producerDelay, TimeUnit.MINUTES);
+        buyerHubScheduledFuture = exec.scheduleWithFixedDelay(new BuyerHub(this), 0, Hub.buyerDelay, TimeUnit.MINUTES);
+        itemHubScheduledFuture = exec.scheduleWithFixedDelay(new ItemHub(this), 0, Hub.itemDelay, TimeUnit.MINUTES);
+        orderHubScheduledFuture = exec.scheduleWithFixedDelay(new OrderHub(this), 0, Hub.orderDelay, TimeUnit.MINUTES);
+        producerHubScheduledFuture = exec.scheduleWithFixedDelay(new ProducerHub(this), 0, Hub.producerDelay, TimeUnit.MINUTES);
+        Log.w(Tag, "startHubThreads exec.getQueue().size() = " + exec.getQueue().size());
+    }
+
+    private void stopHubThreads() {
+        Log.w(Tag, "stopHubThreads exec.getQueue().size() = " + exec.getQueue().size());
+        if (exec.getQueue().size() != 0) {
+            buyerHubScheduledFuture.cancel(false);
+            itemHubScheduledFuture.cancel(false);
+            orderHubScheduledFuture.cancel(false);
+            producerHubScheduledFuture.cancel(false);
+        }
+        exec.shutdownNow();
+        exec = null;
+        exec = new ScheduledThreadPoolExecutor(4);
+        Log.w(Tag, "stopHubThreads exec.getQueue().size() = " + exec.getQueue().size());
+    }
+
+    private void addTab(Class tabClass, int stringId) {
+        ActionBar actionBar = getSupportActionBar();
+        ActionBar.TabListener tabListener = new TabListener(this, tabClass);
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText(stringId);
+        tab.setTabListener(tabListener);
+        actionBar.addTab(tab);
     }
 
     public void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-
-        // init all hubs which are all individual scheduled threads
-        initGetHubs(this.getApplicationContext());
-
-//        setContentView(R.layout.main_activity);
+        super.onCreate(savedInstanceState);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -57,16 +78,34 @@ public class MainActivity extends ActionBarActivity {
         ViewServer.get(this).addWindow(this);
     }
 
-    private void addTab(Class tabClass, int stringId) {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
-        ActionBar actionBar = getSupportActionBar();
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 
-        ActionBar.TabListener tabListener = new TabListener(this, tabClass);
-        ActionBar.Tab tab = actionBar.newTab();
+    @Override
+    public void onResume() {
+        super.onResume();
+        startHubThreads();
+        Log.w(Tag, "Scheduled hub refreshes...");
+        ViewServer.get(this).setFocusedWindow(this);
+    }
 
-        tab.setText(stringId);
-        tab.setTabListener(tabListener);
-        actionBar.addTab(tab);
+    @Override
+    protected void onPause() {
+        stopHubThreads();
+        Log.w(Tag, "Stopped hub refreshes...");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 
     @Override
@@ -75,14 +114,7 @@ public class MainActivity extends ActionBarActivity {
         ViewServer.get(this).removeWindow(this);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        ViewServer.get(this).setFocusedWindow(this);
-    }
-
     public void onClick(View view) {
-
         AsyncTesterTask asyncTesterTask = new AsyncTesterTask(this);
         asyncTesterTask.execute(null);
    }
