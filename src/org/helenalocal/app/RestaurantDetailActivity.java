66564af735d918a6838b355cc1baefbd12d409c9
shane <ganeae.birrinus@gmail.com>
2014-02-11 16:59:46 -0700

    Dynamically loading restaurant image on detail screen

--- src/org/helenalocal/app/RestaurantDetailActivity.java
+++ src/org/helenalocal/app/RestaurantDetailActivity.java
@@ -5,19 +5,25 @@
 package org.helenalocal.app;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.util.Linkify;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.helenalocal.Helena_Local_Hub.R;
 import org.helenalocal.base.Buyer;
 import org.helenalocal.base.Hub;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RestaurantDetailActivity extends Activity {
 
-    private static final String Tag = "RestaurantDetailActivity";
+    private static final String LogTag = "RestaurantDetailActivity";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +33,15 @@ public class RestaurantDetailActivity extends Activity {
         String buyerId = getIntent().getStringExtra(RestaurantTab.BUYER_ID_KEY);
         final Buyer b = Hub.buyerMap.get(buyerId);
 
+        // restaurant name
         TextView tv = (TextView) findViewById(R.id.nameTextView);
         tv.setText(b.getName());
 
+        // restaurant address
+        tv = (TextView)findViewById(R.id.addressTextView);
+        tv.setText(b.getLocation());
+
+        // set clickable link for phone number
         tv = (TextView)findViewById(R.id.callTextView);
         Linkify.addLinks(tv, Pattern.compile(tv.getText().toString()), "tel:", null, new Linkify.TransformFilter() {
             @Override
@@ -38,12 +50,55 @@ public class RestaurantDetailActivity extends Activity {
             }
         });
 
+        // setup clickable link for web address
         tv = (TextView)findViewById(R.id.urlTextView);
         Linkify.addLinks(tv, Pattern.compile(tv.getText().toString()), "http://", null, new Linkify.TransformFilter() {
             @Override
             public String transformUrl(Matcher match, String url) {
-                return b.getWebsiteUrl();
+                String website = b.getWebsiteUrl();
+                if (website.startsWith("http://") == true) {
+                    website = website.substring("http://".length(), website.length());
+                }
+
+                return website;
             }
         });
+
+        // load image
+        ImageView iv = (ImageView)findViewById(R.id.restaurantImageView);
+        new ImageAsyncTask(iv).execute(b.getPhotoUrl());
+    }
+
+    private class ImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
+
+        private ImageView _imageView;
+
+        public ImageAsyncTask(ImageView view) {
+            _imageView = view;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String url = params[0];
+            Bitmap bm = null;
+
+            try {
+                InputStream stream = new URL(url).openStream();
+                bm = BitmapFactory.decodeStream(stream);
+                stream.close();
+            } catch (Exception e) {
+
+            }
+
+            return bm;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null) {
+                _imageView.setImageBitmap(bitmap);
+            }
+        }
     }
 }
