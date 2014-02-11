fae72c4911ae019c8adf86a5cdd5e8b0a4a79723
shane <ganeae.birrinus@gmail.com>
2014-02-11 12:25:15 -0700

    Initial pass of restaurant detail layout

--- src/org/helenalocal/app/RestaurantDetailActivity.java
+++ src/org/helenalocal/app/RestaurantDetailActivity.java
@@ -6,10 +6,14 @@ package org.helenalocal.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.TextView;
 import org.helenalocal.Helena_Local_Hub.R;
 import org.helenalocal.base.Buyer;
 import org.helenalocal.base.Hub;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RestaurantDetailActivity extends Activity {
 
@@ -21,9 +25,25 @@ public class RestaurantDetailActivity extends Activity {
         setContentView(R.layout.restaurant_detail_activity);
 
         String buyerId = getIntent().getStringExtra(RestaurantTab.BUYER_ID_KEY);
-        Buyer b = Hub.buyerMap.get(buyerId);
+        final Buyer b = Hub.buyerMap.get(buyerId);
 
         TextView tv = (TextView) findViewById(R.id.nameTextView);
         tv.setText(b.getName());
+
+        tv = (TextView)findViewById(R.id.callTextView);
+        Linkify.addLinks(tv, Pattern.compile(tv.getText().toString()), "tel:", null, new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return b.getPhone();
+            }
+        });
+
+        tv = (TextView)findViewById(R.id.urlTextView);
+        Linkify.addLinks(tv, Pattern.compile(tv.getText().toString()), "http://", null, new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return b.getWebsiteUrl();
+            }
+        });
     }
 }
